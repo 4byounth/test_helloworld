7a90d42c569ac5e3124e277f34d2319f1e6c8545
我的mac <4byounth@4byounthdeMac.local>
2018-11-13 19:40:16 +0800

    Initial Commit

---
+++ test_helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_helloworld
+//
+//  Created by 4byounth on 2018/11/13.
+//  Copyright © 2018 4byounth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
